4e2e629f7115e9083b286532f7dddb66c1db30a0
Денис Воронков <lil.mozarh@MacBook-Air-Denis.local>
2024-10-08 17:11:14 +0300

    version1

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,28 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+const addsContainer = document.querySelector('.places__list');
+
+function addCards(initialCards, deleteCallback) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+    cardElement.querySelector('.card__title').innerText = initialCards.name;
+    cardElement.querySelector('.card__image').src = initialCards.link;
+
+    cardElement.querySelector('.card__delete-button').addEventListener('click', () =>{
+        deleteCallback(cardElement);
+    });
+
+    return cardElement;
+}
+
+function removeCard(cardElement) {
+    addsContainer.removeChild(cardElement);
+}
+
+initialCards.forEach((cardData) =>{
+    addsContainer.append(addCards(cardData, removeCard));
+});
